6ad2f707646368e9041e5c8eaaf15a0f76fb2d17
Kamal Singh <kamalsingh200238@gmail.com>
2022-08-27 01:39:35 +0530

    Created a function which calls api and used it in Csr

--- pages/csr/index.jsx
+++ pages/csr/index.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import allPokemonData from "../../lib/allPokemonData.js";
+
 export default function Csr() {
-  return (
-    <>
-      <div>This page is generated on the client side</div>
-    </>
-  );
+  //state to store pokemon data
+  const [pokemon, setPokemon] = useState([]);
+
+  //Gett data form api
+  useEffect(() => {
+    async function getData() {
+      setPokemon(await allPokemonData());
+    }
+    getData();
+  }, []);
+
+  // console.log(pokemon);
+
+  if (pokemon.results) {
+    return (
+      <>
+        <div>This page is generated on the client side</div>
+      </>
+    );
+  } else {
+    return <div> Loading...</div>;
+  }
 }
